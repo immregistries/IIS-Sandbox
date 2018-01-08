581c4173607bf3c1576207507d6af74e97a66c05
Eric Ostrom <emostrom@charter.net>
2018-01-08 14:49:08 -0700

    adjust pom and add to message handler

--- src/main/java/org/immregistries/iis/kernal/logic/IncomingMessageHandler.java
+++ src/main/java/org/immregistries/iis/kernal/logic/IncomingMessageHandler.java
@@ -7,13 +7,25 @@ public class IncomingMessageHandler {
     HL7Reader reader = new HL7Reader(message);
     String nameFirst = "";
     String nameLast = "";
+    String address = "";
+    String city = "";
+    String state = "";
+    String zip = "";
     if (reader.advanceToSegment("PID"))
     {
       nameLast = reader.getValue(5, 1);
       nameFirst = reader.getValue(5, 2);
+      address = reader.getValue(11, 1);
+      city = reader.getValue(11, 3);
+      state = reader.getValue(11, 4);
+      zip = reader.getValue(11, 5);
     }
     System.out.println("--> nameFirst = " + nameFirst);
     System.out.println("--> nameLast = " + nameLast);
+    System.out.println("--> adress = " + address);
+    System.out.println("--> city = " + city);
+    System.out.println("--> state = " + state);
+    System.out.println("--> zip = " + zip);
     
     // do stuff here to put it in the database
     return "MSH|^~\\&|DCS|MYIIS|MYIIS||20090604000020-0500||ACK^V04^ACK|1234567|P|2.5.1|||NE|NE|||||Z23^CDCPHINVS\r"
