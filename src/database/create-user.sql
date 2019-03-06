c712af673da1dea340d1c3070ee7ee74c262b312
Kevin Murset <kevinmurset@gmail.com>
2019-03-06 16:03:22 -0700

    Updating create-user.sql script to use 'mysql_native_password' method

--- src/database/create-user.sql
+++ src/database/create-user.sql
@@ -1,3 +1,5 @@
-CREATE USER 'iis_web'@'localhost' IDENTIFIED BY 'SharkBaitHooHaHa';
-
-GRANT ALL PRIVILEGES ON iis.* TO 'iis_web'@'localhost' WITH GRANT OPTION;
+CREATE USER
+  'iis_web'@'localhost' IDENTIFIED WITH mysql_native_password
+                                   BY 'SharkBaitHooHaHa';
+								   
+GRANT ALL ON iis.* TO 'iis_web'@'localhost';
