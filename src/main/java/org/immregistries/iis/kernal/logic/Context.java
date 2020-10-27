2ee2d6a398cc3762719b4b371fd3fcd7109b5f6b
Clément <clement.hennequin@telecomnancy.eu>
2020-10-27 14:59:57 +0100

    Context final

--- src/main/java/org/immregistries/iis/kernal/logic/Context.java
+++ src/main/java/org/immregistries/iis/kernal/logic/Context.java
@@ -3,7 +3,7 @@ package org.immregistries.iis.kernal.logic;
 import ca.uhn.fhir.context.FhirContext;
 
 public class Context {
-    private static FhirContext ctx = FhirContext.forR4();
+    private static final FhirContext ctx = FhirContext.forR4();
 
     public static FhirContext getCtx() {
         return ctx;
