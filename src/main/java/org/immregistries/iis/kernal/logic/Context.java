d61028cd95d6d02e50a62c2e28ca4d99419e0209
Clément <clement.hennequin@telecomnancy.eu>
2020-10-28 22:47:31 +0100

    Adding parsers to fhir ressources

--- src/main/java/org/immregistries/iis/kernal/logic/Context.java
+++ src/main/java/org/immregistries/iis/kernal/logic/Context.java
@@ -1,11 +1,22 @@
 package org.immregistries.iis.kernal.logic;
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
 
 public class Context {
     private static final FhirContext ctx = FhirContext.forR4();
+    private static final IParser xml = ctx.newXmlParser();
+    private static final IParser json = ctx.newJsonParser();
 
     public static FhirContext getCtx() {
         return ctx;
+    }
+
+    public static IParser fhir_parser(String message){ //Returns appropriate parser
+        if (message.charAt(0) == '<'){ //If XML
+            return xml;
+        }else{ //else should be json
+            return json;
+        }
     }
 }
