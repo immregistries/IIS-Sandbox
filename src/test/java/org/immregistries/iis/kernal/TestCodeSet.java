83be2bd659cb5f2dc30d09b7d1aa762cec481644
Nathan Bunker <nbunker@immregistries.org>
2020-11-21 06:57:29 -0700

    Create TestCodeSet.java
    Added test to verify that the code set is working

---
+++ src/test/java/org/immregistries/iis/kernal/TestCodeSet.java
@@ -0,0 +1,19 @@
+package org.immregistries.iis.kernal;
+
+import org.immregistries.codebase.client.CodeMap;
+import org.immregistries.codebase.client.generated.Code;
+import org.immregistries.codebase.client.reference.CodesetType;
+import org.immregistries.iis.kernal.logic.CodeMapManager;
+import junit.framework.TestCase;
+
+public class TestCodeSet extends TestCase  {
+
+  public void testCodeMapManager()
+  {
+    CodeMap codeMap = CodeMapManager.getCodeMap();
+    Code code = codeMap.getCodeForCodeset(CodesetType.VACCINATION_CVX_CODE, "03");
+    assertNotNull(code);
+    assertEquals("03", code.getValue());
+    assertEquals("MMR", code.getLabel());
+  }
+}
