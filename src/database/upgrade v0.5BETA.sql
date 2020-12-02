44d2cd6f6e3f719f2e98ee5063834d5b17398f6b
babforlife <55501953+babforlife@users.noreply.github.com>
2020-12-02 15:09:10 +0100

    Update upgrade v0.5BETA.sql

--- src/database/upgrade v0.5BETA.sql
+++ src/database/upgrade v0.5BETA.sql
@@ -1,6 +1,7 @@
-CREATE TABLE patientlink
+CREATE TABLE patient_link
 (
-  patient_master_ID varchar(60)
-  patient_reported_ID varchar(60)
+  id integer primary key auto_increment,
+  patient_master_ID varchar(60),
+  patient_reported_ID varchar(60),
   level_confidence integer
 );
