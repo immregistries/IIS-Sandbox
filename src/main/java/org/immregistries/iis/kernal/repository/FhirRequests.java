13af2c5e68ddcd1c98fb2bc0dd356651ebe0805f
clement <clement.hennequin@nist.gov>
2022-08-22 18:27:47 -0400

    HAPI fhir TokenCriterion class made public, fixed conditionnal updates

--- src/main/java/org/immregistries/iis/kernal/repository/FhirRequests.java
+++ src/main/java/org/immregistries/iis/kernal/repository/FhirRequests.java
@@ -1,14 +1,9 @@
 package org.immregistries.iis.kernal.repository;
 
 import ca.uhn.fhir.jpa.api.dao.IFhirResourceDao;
-import ca.uhn.fhir.jpa.searchparam.SearchParameterMap;
 import ca.uhn.fhir.rest.api.MethodOutcome;
-import ca.uhn.fhir.rest.api.server.IBundleProvider;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.gclient.*;
-import ca.uhn.fhir.rest.param.StringParam;
-import ca.uhn.fhir.rest.param.TokenParam;
-import ca.uhn.fhir.rest.param.TokenParamModifier;
 import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r5.model.*;
@@ -22,7 +17,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.immregistries.iis.kernal.mapping.MappingHelper.OBSERVATION_REPORTED;
 import static org.immregistries.iis.kernal.mapping.MappingHelper.PATIENT_REPORTED;
@@ -34,15 +28,15 @@ public class FhirRequests {
 	@Autowired
 	RepositoryClientFactory repositoryClientFactory;
 
-	@Autowired
-	protected IFhirResourceDao<org.hl7.fhir.r5.model.Patient> myPatientDao;
+
 
 	Logger logger = LoggerFactory.getLogger(FhirRequests.class);
 
 
-	private static final String GOLDEN_SYSTEM = "http://hapifhir.io/fhir/NamingSystem/mdm-golden-resource-enterprise-id";
+	private static final String GOLDEN_SYSTEM_TAG = "http://hapifhir.io/fhir/NamingSystem/mdm-record-status";
 	private static final String GOLDEN_RECORD = "GOLDEN_RECORD";
-	private static final String GOLDEN_TAG = GOLDEN_SYSTEM +"|" + GOLDEN_RECORD;
+	private static final TokenCriterion NOT_GOLDEN_CRITERION= new TokenCriterion("_tag:not",GOLDEN_SYSTEM_TAG,GOLDEN_RECORD);
+
 
 	public PatientMaster searchPatientMaster(IGenericClient fhirClient, ICriterion... where) {
 		PatientMaster patientMaster = null;
@@ -55,12 +49,6 @@ public class FhirRequests {
 	public PatientReported searchPatientReported(IGenericClient fhirClient, ICriterion... where) {
 		PatientReported patientReported = null;
 		Bundle bundle = searchRegularRecord(Patient.class,fhirClient, where);
-//		SearchParameterMap map = new SearchParameterMap().add("_tag",new TokenParam().setSystem(GOLDEN_SYSTEM).setValue(GOLDEN_RECORD).setModifier(TokenParamModifier.NOT));
-//		IBundleProvider bundle1 = myPatientDao.search(map);
-//		if (!bundle1.isEmpty()) {
-//			patientReported = PatientMapper.getReported((Patient) bundle.getEntryFirstRep().getResource());
-//		}
-
 		if (bundle.hasEntry()) {
 			patientReported = PatientMapper.getReported((Patient) bundle.getEntryFirstRep().getResource());
 		}
@@ -69,11 +57,6 @@ public class FhirRequests {
 
 	public List<PatientReported> searchPatientReportedList(IGenericClient fhirClient, ICriterion... where) {
 		List<PatientReported> patientReportedList = new ArrayList<>();
-//		SearchParameterMap map = new SearchParameterMap().add("_tag",new TokenParam().setSystem(GOLDEN_SYSTEM).setValue(GOLDEN_RECORD).setModifier(TokenParamModifier.NOT));
-//		IBundleProvider bundle1 = myPatientDao.search(map, th);
-//		for (IBaseResource entry: bundle1.getAllResources()) {
-//			patientReportedList.add(PatientMapper.getReported((Patient) entry));
-//		}
 		Bundle bundle = searchRegularRecord(Patient.class,fhirClient, where);
 		for (Bundle.BundleEntryComponent entry: bundle.getEntry()) {
 			patientReportedList.add(PatientMapper.getReported((Patient) entry.getResource()));
@@ -147,7 +130,7 @@ public class FhirRequests {
 			IQuery<Bundle> query = fhirClient.search().forResource(aClass).returnBundle(Bundle.class);
 			int size = where.length;
 			if (size > 0) {
-				query = query.where(where[0]).withTag(GOLDEN_SYSTEM, GOLDEN_RECORD);
+				query = query.where(where[0]).withTag(GOLDEN_SYSTEM_TAG, GOLDEN_RECORD);
 			}
 			int i = 1;
 			while (i < size) {
@@ -166,10 +149,8 @@ public class FhirRequests {
 		try {
 			IQuery<Bundle> query = fhirClient.search().forResource(aClass).returnBundle(Bundle.class);
 			int size = where.length;
-			if (size > 0) {
-				query = query.where(where[0]).withTag(":not"+GOLDEN_SYSTEM, GOLDEN_RECORD);
-			}
-			int i = 1;
+			query = query.where(NOT_GOLDEN_CRITERION);
+			int i = 0;
 			while (i < size) {
 				query = query.and(where[i]);
 				i++;
@@ -202,8 +183,6 @@ public class FhirRequests {
 
 	public PatientReported savePatientReported(IGenericClient fhirClient, PatientReported patientReported) {
 		Patient patient =  PatientMapper.getFhirResource( null,patientReported);
-//		Patient patient = new Patient();
-//		PatientHandler.fillFhirResource(patient, null,patientReported);
 		MethodOutcome outcome = saveRegular(fhirClient,patient,
 			Patient.IDENTIFIER.exactly().systemAndIdentifier(PATIENT_REPORTED,patientReported.getPatientReportedExternalLink()));
 		if (outcome.getCreated() != null && outcome.getCreated()) {
@@ -256,19 +235,15 @@ public class FhirRequests {
 	private MethodOutcome saveRegular(IGenericClient fhirClient, IBaseResource resource, ICriterion... where) {
 		MethodOutcome outcome;
 		try {
-
 			IUpdateTyped query = fhirClient.update().resource(resource);
 			int size = where.length;
-			if (size > 0) {
-				query = query.conditional().where(where[0]);
-			}
-			int i = 1;
+			query = query.conditional().where(NOT_GOLDEN_CRITERION);
+			int i = 0;
 			while (i < size) {
 				query = ((IUpdateWithQuery) query).and(where[i]);
 				i++;
 			}
 			outcome = query
-//				.conditionalByUrl(resource.fhirType() + "?_tag=:not=" + GOLDEN_TAG)
 				.execute();
 		} catch (ResourceNotFoundException e){
 			outcome = fhirClient.create().resource(resource)
