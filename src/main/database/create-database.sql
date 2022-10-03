fd4e9e4b62c21ce46d5aac518671a5535fa83a5e
clement <clement.hennequin@nist.gov>
2022-10-03 15:48:04 -0400

    Database for login script

---
+++ src/main/database/create-database.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE iis_alongside_jpa;
+
+USE iis_alongside_jpa;
+
+CREATE USER
+  'iis_web'@'localhost' IDENTIFIED WITH mysql_native_password
+                                   BY 'SharkBaitHooHaHa';
+
+GRANT ALL ON iis_alongside_jpa.* TO 'iis_web'@'localhost';
+
+DROP TABLE IF EXISTS `message_received`;
+CREATE TABLE `message_received` (
+  `message_received_id` int NOT NULL AUTO_INCREMENT,
+  `org_id` int NOT NULL,
+  `message_request` mediumtext NOT NULL,
+  `message_response` mediumtext NOT NULL,
+  `reported_date` datetime NOT NULL,
+  `category_request` varchar(250) NOT NULL,
+  `category_response` varchar(250) NOT NULL,
+  `patient_reported_id` int DEFAULT NULL,
+  PRIMARY KEY (`message_received_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DROP TABLE IF EXISTS `org_master`;
+CREATE TABLE `org_master` (
+  `org_id` int NOT NULL AUTO_INCREMENT,
+  `organization_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`org_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DROP TABLE IF EXISTS `org_access`;
+CREATE TABLE `org_access` (
+  `org_access_id` int NOT NULL AUTO_INCREMENT,
+  `org_id` int NOT NULL,
+  `access_name` varchar(30) NOT NULL,
+  `access_key` varchar(250) NOT NULL,
+  PRIMARY KEY (`org_access_id`),
+  KEY `org_id` (`org_id`),
+  CONSTRAINT `org_access_ibfk_1` FOREIGN KEY (`org_id`) REFERENCES `Org_Master` (`org_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
