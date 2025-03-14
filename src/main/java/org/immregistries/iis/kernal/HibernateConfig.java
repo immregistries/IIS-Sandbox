6c49e33e08d40c5e48562d70c92afeac363d8509
Clement Hennequin <clement.hennequin@nist.gov>
2025-03-14 16:23:41 -0400

    Attempt at using H2, not ready

--- src/main/java/org/immregistries/iis/kernal/HibernateConfig.java
+++ src/main/java/org/immregistries/iis/kernal/HibernateConfig.java
@@ -19,10 +19,9 @@ public class HibernateConfig {
 		/*
 		 * For deploying time configuration with ENV variable
 		 */
-		String database_url = System.getenv("IIS_MYSQL_URL");
-		logger.info("DATABASE_URL {}", database_url);
-		String database_user = System.getenv("ENV_IIS_MYSQL_USER");
-		String database_password = System.getenv("ENV_IIS_MYSQL_PASSWORD");
+		String database_url = getSystemVariableFromEnvOrProperty("IIS_MYSQL_URL");
+		String database_user = getSystemVariableFromEnvOrProperty("ENV_IIS_MYSQL_USER");
+		String database_password = getSystemVariableFromEnvOrProperty("ENV_IIS_MYSQL_PASSWORD");
 		if (StringUtils.isNotBlank(database_url)) {
 			cfg.setProperty("hibernate.connection.url", database_url);
 			if (database_url.startsWith("jdbc:h2:")) {
@@ -31,6 +30,7 @@ public class HibernateConfig {
 			} else {
 				cfg.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
 				cfg.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+				cfg.setProperty("hibernate.ddl-auto", "create");
 			}
 		}
 		if (StringUtils.isNotBlank(database_user)) {
@@ -53,6 +53,14 @@ public class HibernateConfig {
 		cfg.addResource("org/immregistries/iis/kernal/model/UserAccess.hbm.xml");
 		cfg.addResource("org/immregistries/iis/kernal/model/MessageReceived.hbm.xml");
 		return cfg;
+	}
+
+	private static String getSystemVariableFromEnvOrProperty(String variableName) {
+		String database_url = System.getenv(variableName);
+		if (StringUtils.isBlank(database_url)) {
+			database_url = System.getProperty(variableName);
+		}
+		return database_url;
 	}
 
 	public static SessionFactory sessionFactory() {
