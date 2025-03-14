8fb82f24dd1ce10eab3bb61765ce902965c551e6
Clement Hennequin <clement.hennequin@nist.gov>
2025-03-14 14:50:59 -0400

    Configuration with .env 4, working, to be improved for clarity

--- src/main/java/org/immregistries/iis/kernal/HibernateConfig.java
+++ src/main/java/org/immregistries/iis/kernal/HibernateConfig.java
@@ -1,6 +1,7 @@
 package org.immregistries.iis.kernal;
 
 import org.apache.commons.lang3.StringUtils;
+import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 /**
@@ -18,6 +19,14 @@ public class HibernateConfig {
 		String database_password = System.getenv("ENV_IIS_MYSQL_PASSWORD");
 		if (StringUtils.isNotBlank(database_url)) {
 			cfg.setProperty("hibernate.connection.url", database_url);
+			System.out.println(database_url);
+			if (StringUtils.isBlank(cfg.getProperty("hibernate.dialect"))) {
+				if (database_url.startsWith("jdbc:mysql:")) {
+					cfg.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+				} else if (database_url.startsWith("jdbc:h2:")) {
+					cfg.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+				}
+			}
 		}
 		if (StringUtils.isNotBlank(database_user)) {
 			cfg.setProperty("hibernate.connection.username", database_user);
@@ -25,7 +34,11 @@ public class HibernateConfig {
 		if (StringUtils.isNotBlank(database_password)) {
 			cfg.setProperty("hibernate.connection.password", database_password);
 		}
-
 		return cfg;
+	}
+
+	public static SessionFactory sessionFactory() {
+		SessionFactory sessionFactory = configuration().buildSessionFactory();
+		return sessionFactory;
 	}
 }
