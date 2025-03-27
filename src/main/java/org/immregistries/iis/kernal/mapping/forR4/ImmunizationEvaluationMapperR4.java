a5037044220be3f2c0bf01be3bb8634f961417c2
Clement Hennequin <clement.hennequin@nist.gov>
2025-03-27 15:37:22 -0400

    Progress on ImmunizationEvaluationMapperR4

--- src/main/java/org/immregistries/iis/kernal/mapping/forR4/ImmunizationEvaluationMapperR4.java
+++ src/main/java/org/immregistries/iis/kernal/mapping/forR4/ImmunizationEvaluationMapperR4.java
@@ -1,12 +1,18 @@
 package org.immregistries.iis.kernal.mapping.forR4;
 
+import org.apache.commons.lang3.StringUtils;
+import org.hl7.fhir.r4.model.CodeableConcept;
+import org.hl7.fhir.r4.model.Coding;
 import org.hl7.fhir.r4.model.ImmunizationEvaluation;
 import org.hl7.fhir.r4.model.Reference;
 import org.immregistries.codebase.client.CodeMap;
+import org.immregistries.codebase.client.generated.Code;
+import org.immregistries.codebase.client.reference.CodesetType;
 import org.immregistries.iis.kernal.fhir.common.annotations.OnR4Condition;
 import org.immregistries.iis.kernal.logic.CodeMapManager;
 import org.immregistries.iis.kernal.model.VaccinationMaster;
 import org.immregistries.vfa.connect.model.EvaluationActual;
+import org.immregistries.vfa.connect.model.TestEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Service;
@@ -29,11 +35,20 @@ public class ImmunizationEvaluationMapperR4 {
 		immunizationEvaluation.setImmunizationEvent(new Reference("Immunization/" + vaccinationMaster.getVaccinationId()));
 
 		if (vaccinationMaster.getTestEvent() != null && vaccinationMaster.getTestEvent().getEvaluationActualList() != null) {
-
+			TestEvent testEvent = vaccinationMaster.getTestEvent();
+//			testEvent.get
 			immunizationEvaluation.setStatus(ImmunizationEvaluation.ImmunizationEvaluationStatus.COMPLETED);
-			for (EvaluationActual evaluationActual : vaccinationMaster.getTestEvent().getEvaluationActualList()) {
+			for (EvaluationActual evaluationActual : testEvent.getEvaluationActualList()) {
 				immunizationEvaluation.setSeries(evaluationActual.getSeriesUsedCode());
+				String cvx = evaluationActual.getVaccineCvx();
+				if (StringUtils.isBlank(cvx)) {
+					cvx = evaluationActual.getVaccineGroup().getVaccineCvx();
+				}
+				Code cvxCode = codeMap.getCodeForCodeset(CodesetType.VACCINATION_CVX_CODE, cvx);
+				immunizationEvaluation.setTargetDisease(new CodeableConcept().addCoding(new Coding("cvx", cvx, cvxCode.getLabel())));
 			}
+
+			immunizationEvaluation.setDescription(testEvent.getLabelScreen());
 		} else {
 			return null;
 		}
