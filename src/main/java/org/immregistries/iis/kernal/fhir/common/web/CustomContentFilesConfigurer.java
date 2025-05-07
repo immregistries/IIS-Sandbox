e7d8c1abeb1826f18a8cbd65bcfab3a6acc4af3f
Clement Hennequin <clement.hennequin@nist.gov>
2025-05-07 15:51:51 -0400

    Added new Web configuration, security config TODO

---
+++ src/main/java/org/immregistries/iis/kernal/fhir/common/web/CustomContentFilesConfigurer.java
@@ -0,0 +1,39 @@
+package org.immregistries.iis.kernal.fhir.common.web;
+
+import org.immregistries.iis.kernal.fhir.common.AppProperties;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileUrlResource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.net.MalformedURLException;
+
+@Configuration
+@ConditionalOnProperty(prefix = "hapi.fhir", name = "custom_content_path")
+public class CustomContentFilesConfigurer implements WebMvcConfigurer {
+
+	public static final String CUSTOM_CONTENT = "/content";
+	private String customContentPath;
+
+	public CustomContentFilesConfigurer(AppProperties appProperties) {
+		customContentPath = appProperties.getCustom_content_path();
+		if (customContentPath.endsWith("/"))
+			customContentPath = customContentPath.substring(0, customContentPath.lastIndexOf('/'));
+	}
+
+	@Override
+	public void addResourceHandlers(@NotNull ResourceHandlerRegistry theRegistry) {
+		if (!theRegistry.hasMappingForPattern(CUSTOM_CONTENT + "/**")) {
+
+			try {
+				theRegistry
+					.addResourceHandler(CUSTOM_CONTENT + "/**")
+					.addResourceLocations(new FileUrlResource(customContentPath));
+			} catch (MalformedURLException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+}
--- src/main/java/org/immregistries/iis/kernal/fhir/security/ServerSecurityConfig.java
+++ src/main/java/org/immregistries/iis/kernal/fhir/security/ServerSecurityConfig.java
@@ -4,12 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.immregistries.iis.kernal.servlet.LoginServlet.PARAM_PASSWORD;
-import static org.immregistries.iis.kernal.servlet.LoginServlet.PARAM_USERID;
 
 
 @Configuration
@@ -18,35 +14,35 @@ public class ServerSecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http, CustomOAuthSuccessHandler customOAuthSuccessHandler) throws Exception {
-		http
-			.authorizeRequests()
-			.antMatchers(HttpMethod.GET, "/", "/home", "/pop", "/SubscriptionTopic/**", "/img/**").permitAll()
-			.antMatchers("/loginForm", "/oauth2/**", "/login" ).permitAll()
-			// API AUTHORIZATION AND AUTHENTICATION SEPARATED
-			.antMatchers("/fhir/**", "/soap","/.well-known/smart-configuration","/registerClient", "/token").permitAll()
-			.anyRequest().authenticated()
-			.and()
-			//USERNAME
-			.formLogin()
-			.usernameParameter(PARAM_USERID)
-			.passwordParameter(PARAM_PASSWORD)
-			.loginPage("/loginForm") // Page where redirected when unauthorised
-			.loginProcessingUrl("/login") // url for login request to be processed (hollow)
-			.defaultSuccessUrl("/home")
-			.and()
-			//OAUTH
-			.oauth2Login()
-			.defaultSuccessUrl("/home")
-			.successHandler(customOAuthSuccessHandler)
-			.and()
-			// LOGOUT
-			.logout()
-			.logoutUrl("/logout")
-			.logoutSuccessUrl("/loginForm")
-			.deleteCookies("JSESSIONID")
-			.and()
-			.csrf()
-			.disable()
+//		http
+//			.authorizeRequests()
+//			.antMatchers(HttpMethod.GET, "/", "/home", "/pop", "/SubscriptionTopic/**", "/img/**").permitAll()
+//			.antMatchers("/loginForm", "/oauth2/**", "/login" ).permitAll()
+//			// API AUTHORIZATION AND AUTHENTICATION SEPARATED
+//			.antMatchers("/fhir/**", "/soap","/.well-known/smart-configuration","/registerClient", "/token").permitAll()
+//			.anyRequest().authenticated()
+//			.and()
+//			//USERNAME
+//			.formLogin()
+//			.usernameParameter(PARAM_USERID)
+//			.passwordParameter(PARAM_PASSWORD)
+//			.loginPage("/loginForm") // Page where redirected when unauthorised
+//			.loginProcessingUrl("/login") // url for login request to be processed (hollow)
+//			.defaultSuccessUrl("/home")
+//			.and()
+//			//OAUTH
+//			.oauth2Login()
+//			.defaultSuccessUrl("/home")
+//			.successHandler(customOAuthSuccessHandler)
+//			.and()
+//			// LOGOUT
+//			.logout()
+//			.logoutUrl("/logout")
+//			.logoutSuccessUrl("/loginForm")
+//			.deleteCookies("JSESSIONID")
+//			.and()
+//			.csrf()
+//			.disable()
 //				.ignoringAntMatchers("/pop","/message", "/fhir/**", "/loginForm", "/logout",  "/patient", "/subscription", "/vaccination")
 		;
 		// ... other configuration
