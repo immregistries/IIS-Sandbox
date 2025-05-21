794c5fa079fc9c30f545a595bbb03fbdbcfb56d6
Clement Hennequin <clement.hennequin@nist.gov>
2025-05-21 14:27:36 -0400

    Redundancy in RecommendationController

--- src/main/java/org/immregistries/iis/kernal/servlet/RecommendationController.java
+++ src/main/java/org/immregistries/iis/kernal/servlet/RecommendationController.java
@@ -35,13 +35,10 @@ import static org.immregistries.iis.kernal.servlet.RecommendationController.RECO
 @RequestMapping({RECOMMENDATION_BASE_PATH, TenantController.TENANT_PATH + RECOMMENDATION_BASE_PATH})
 public class RecommendationController {
 	public static final String RECOMMENDATION_BASE_PATH = "/recommendation";
-
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
 	public static final String PARAM_RECOMMENDATION_ID = "recommendationId";
 	public static final String PARAM_RECOMMENDATION_IDENTIFIER = "recommendationIdentifier";
 	public static final String PARAM_RECOMMENDATION_RESOURCE = "recommendationResource";
-
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	private IImmunizationRecommendationService immunizationRecommendationService;
 	@Autowired
@@ -53,13 +50,14 @@ public class RecommendationController {
 	@Autowired
 	private PatientMapper patientMapper;
 
+
 	/**
 	 * Used to add a random generated component to recommendation
 	 *
-	 * @param req request
+	 * @param req  request
 	 * @param resp response
 	 * @throws ServletException Servlet Exception
-	 * @throws IOException print output stream exception
+	 * @throws IOException      print output stream exception
 	 */
 	@PostMapping
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp
@@ -107,14 +105,11 @@ public class RecommendationController {
 	/**
 	 * Used to manually edit the Recommendation resource
 	 *
-	 * @param req request
+	 * @param req  request
 	 * @param resp response
 	 */
 	@PutMapping
-	protected void doPut(HttpServletRequest req, HttpServletResponse resp
-//		, @PathVariable(name = TenantController.PATH_VARIABLE_TENANT_NAME, required = false) String tenantName dealt with in filter
-	)
-		throws ServletException, IOException {
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		Tenant tenant = ServletHelper.getTenant(req);
 		if (tenant == null) {
 			if (ServletHelper.getUserAccess() != null) {
@@ -160,10 +155,7 @@ public class RecommendationController {
 	 * @throws IOException      OutputStream exception
 	 */
 	@GetMapping
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp
-//		, @PathVariable(name = TenantController.PATH_VARIABLE_TENANT_NAME, required = false) String tenantName dealt with in filter
-	)
-		throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		Tenant tenant = ServletHelper.getTenant(req);
 		if (tenant == null) {
 			if (ServletHelper.getUserAccess() != null) {
@@ -286,101 +278,75 @@ public class RecommendationController {
 	}
 
 	public void printRecommendation(PrintWriter out, IDomainResource recommendation, IDomainResource patient) {
+		out.println("<div class=\"w3-container\">");
+		out.println("<h4>Recommendations</h4>");
+		if (recommendation != null) {
+			out.println("<table class=\"w3-table w3-bordered w3-striped w3-border test w3-hoverable\">");
+			out.println("  <tr class=\"w3-green\">");
+			out.println("    <th>Code</th>");
+			out.println("    <th>Date</th>");
+			out.println("    <th>Date Criterion</th>");
+			out.println("    <th></th>");
+			out.println("  </tr>");
+			out.println("<tbody>");
+
+		} else {
+			out.println("<div class=\"w3-panel w3-yellow\"><p>No Recommendation Found</p></div>");
+			out.println("<form action=\"recommendation\" method=\"POST\">");
+			out.println("	<input type=\"hidden\" name=\"" + PARAM_PATIENT_REPORTED_ID + "\" value=\"" + new org.hl7.fhir.r5.model.IdType(patient.getId()).getIdPart() + "\"/>");
+			out.println("	<input class=\"w3-button w3-section w3-teal w3-ripple\" type=\"submit\" name=\"submit\" value=\"Generate new recommendation\"/>");
+			out.println("</form>");
+		}
 		if (fhirContext.getVersion().getVersion().equals(FhirVersionEnum.R5)) {
-			printRecommendationR5(out, (org.hl7.fhir.r5.model.ImmunizationRecommendation) recommendation, (org.hl7.fhir.r5.model.Patient) patient);
+			printRecommendationR5(out, (org.hl7.fhir.r5.model.ImmunizationRecommendation) recommendation);
 		} else if (fhirContext.getVersion().getVersion().equals(FhirVersionEnum.R4)) {
 			printRecommendationR4(out, (org.hl7.fhir.r4.model.ImmunizationRecommendation) recommendation, (org.hl7.fhir.r4.model.Patient) patient);
 
 		}
+		out.println("</tbody>");
+		out.println("</table>");
 
-	}
-
-	public static void printRecommendationR5(PrintWriter out, org.hl7.fhir.r5.model.ImmunizationRecommendation recommendation, org.hl7.fhir.r5.model.Patient patient) {
-		out.println("<div class=\"w3-container\">");
-		out.println("<h4>Recommendations</h4>");
-		if (recommendation != null) {
-			out.println("<table class=\"w3-table w3-bordered w3-striped w3-border test w3-hoverable\">");
-			out.println("  <tr class=\"w3-green\">");
-			out.println("    <th>Code</th>");
-			out.println("    <th>Date</th>");
-			out.println("    <th>Date Criterion</th>");
-			out.println("    <th></th>");
-			out.println("  </tr>");
-			out.println("<tbody>");
-			int count = 0;
-			for (org.hl7.fhir.r5.model.ImmunizationRecommendation.ImmunizationRecommendationRecommendationComponent component : recommendation.getRecommendation()) {
-				count++;
-				if (count > 100) {
-					break;
-				}
-				String link = "recommendation?" + PARAM_RECOMMENDATION_ID + "="
-					+ new org.hl7.fhir.r5.model.IdType(recommendation.getId()).getIdPart();
-				out.println("<tr>");
-				out.println("    <td><a href=\"" + link + "\">" + component.getVaccineCodeFirstRep().getCodingFirstRep().getCode() + "</a></td>");
-				out.println("    <td><a href=\"" + link + "\">" + component.getDateCriterionFirstRep().getValue() + "</a></td>");
-				out.println("    <td><a href=\"" + link + "\">" + component.getDateCriterionFirstRep().getCode().getCodingFirstRep().getDisplay() + "</a></td>");
-				out.println("</tr>");
-			}
-			out.println("</tbody>");
-			out.println("</table>");
-
-			out.println("<form action=\"recommendation\" method=\"POST\">");
-			out.println("	<input type=\"hidden\" name=\"" + PARAM_PATIENT_REPORTED_ID + "\" value=\"" + new org.hl7.fhir.r5.model.IdType(patient.getId()).getIdPart() + "\"/>");
-			out.println("	<input type=\"hidden\" name=\"" + PARAM_RECOMMENDATION_ID + "\" value=\"" + new org.hl7.fhir.r5.model.IdType(recommendation.getId()).getIdPart() + "\"/>");
-			out.println("	<input class=\"w3-button w3-section w3-teal w3-ripple\" type=\"submit\" name=\"submit\" value=\"Add recommendation component\"/>");
-			out.println("</form>");
-		} else {
-			out.println("<div class=\"w3-panel w3-yellow\"><p>No Recommendation Found</p></div>");
-			out.println("<form action=\"recommendation\" method=\"POST\">");
-			out.println("	<input type=\"hidden\" name=\"" + PARAM_PATIENT_REPORTED_ID + "\" value=\"" + new org.hl7.fhir.r5.model.IdType(patient.getId()).getIdPart() + "\"/>");
-			out.println("	<input class=\"w3-button w3-section w3-teal w3-ripple\" type=\"submit\" name=\"submit\" value=\"Generate new recommendation\"/>");
-			out.println("</form>");
-		}
+		out.println("<form action=\"recommendation\" method=\"POST\">");
+		out.println("	<input type=\"hidden\" name=\"" + PARAM_PATIENT_REPORTED_ID + "\" value=\"" + new org.hl7.fhir.r5.model.IdType(patient.getId()).getIdPart() + "\"/>");
+		out.println("	<input type=\"hidden\" name=\"" + PARAM_RECOMMENDATION_ID + "\" value=\"" + new org.hl7.fhir.r5.model.IdType(recommendation.getId()).getIdPart() + "\"/>");
+		out.println("	<input class=\"w3-button w3-section w3-teal w3-ripple\" type=\"submit\" name=\"submit\" value=\"Add recommendation component\"/>");
+		out.println("</form>");
 		out.println("</div>");
 	}
 
-	// TODO Remove redundancy
+	public static void printRecommendationR5(PrintWriter out, org.hl7.fhir.r5.model.ImmunizationRecommendation recommendation) {
+		int count = 0;
+		for (org.hl7.fhir.r5.model.ImmunizationRecommendation.ImmunizationRecommendationRecommendationComponent component : recommendation.getRecommendation()) {
+			count++;
+			if (count > 100) {
+				break;
+			}
+			String link = "recommendation?" + PARAM_RECOMMENDATION_ID + "="
+				+ new org.hl7.fhir.r5.model.IdType(recommendation.getId()).getIdPart();
+			out.println("<tr>");
+			out.println("    <td><a href=\"" + link + "\">" + component.getVaccineCodeFirstRep().getCodingFirstRep().getCode() + "</a></td>");
+			out.println("    <td><a href=\"" + link + "\">" + component.getDateCriterionFirstRep().getValue() + "</a></td>");
+			out.println("    <td><a href=\"" + link + "\">" + component.getDateCriterionFirstRep().getCode().getCodingFirstRep().getDisplay() + "</a></td>");
+			out.println("</tr>");
+		}
+	}
+
 	public static void printRecommendationR4(PrintWriter out, org.hl7.fhir.r4.model.ImmunizationRecommendation recommendation, org.hl7.fhir.r4.model.Patient patient) {
-		out.println("<div class=\"w3-container\">");
-		out.println("<h4>Recommendations</h4>");
-		if (recommendation != null) {
-			out.println("<table class=\"w3-table w3-bordered w3-striped w3-border test w3-hoverable\">");
-			out.println("  <tr class=\"w3-green\">");
-			out.println("    <th>Code</th>");
-			out.println("    <th>Date</th>");
-			out.println("    <th>Date Criterion</th>");
-			out.println("    <th></th>");
-			out.println("  </tr>");
-			out.println("<tbody>");
-			int count = 0;
-			for (org.hl7.fhir.r4.model.ImmunizationRecommendation.ImmunizationRecommendationRecommendationComponent component : recommendation.getRecommendation()) {
-				count++;
-				if (count > 100) {
-					break;
-				}
-				String link = "recommendation?" + PARAM_RECOMMENDATION_ID + "="
-					+ new org.hl7.fhir.r4.model.IdType(recommendation.getId()).getIdPart();
-				out.println("<tr>");
-				out.println("    <td><a href=\"" + link + "\">" + component.getVaccineCodeFirstRep().getCodingFirstRep().getCode() + "</a></td>");
-				out.println("    <td><a href=\"" + link + "\">" + component.getDateCriterionFirstRep().getValue() + "</a></td>");
-				out.println("    <td><a href=\"" + link + "\">" + component.getDateCriterionFirstRep().getCode().getCodingFirstRep().getDisplay() + "</a></td>");
-				out.println("</tr>");
-			}
-			out.println("</tbody>");
-			out.println("</table>");
+		int count = 0;
 
-			out.println("<form action=\"recommendation\" method=\"POST\">");
-			out.println("	<input type=\"hidden\" name=\"" + PARAM_PATIENT_REPORTED_ID + "\" value=\"" + new org.hl7.fhir.r4.model.IdType(patient.getId()).getIdPart() + "\"/>");
-			out.println("	<input type=\"hidden\" name=\"" + PARAM_RECOMMENDATION_ID + "\" value=\"" + new org.hl7.fhir.r4.model.IdType(recommendation.getId()).getIdPart() + "\"/>");
-			out.println("	<input class=\"w3-button w3-section w3-teal w3-ripple\" type=\"submit\" name=\"submit\" value=\"Add recommendation component\"/>");
-			out.println("</form>");
-		} else {
-			out.println("<div class=\"w3-panel w3-yellow\"><p>No Recommendation Found</p></div>");
-			out.println("<form action=\"recommendation\" method=\"POST\">");
-			out.println("	<input type=\"hidden\" name=\"" + PARAM_PATIENT_REPORTED_ID + "\" value=\"" + new org.hl7.fhir.r4.model.IdType(patient.getId()).getIdPart() + "\"/>");
-			out.println("	<input class=\"w3-button w3-section w3-teal w3-ripple\" type=\"submit\" name=\"submit\" value=\"Generate new recommendation\"/>");
-			out.println("</form>");
+		for (org.hl7.fhir.r4.model.ImmunizationRecommendation.ImmunizationRecommendationRecommendationComponent component : recommendation.getRecommendation()) {
+			count++;
+			if (count > 100) {
+				break;
+			}
+			String link = "recommendation?" + PARAM_RECOMMENDATION_ID + "="
+				+ new org.hl7.fhir.r4.model.IdType(recommendation.getId()).getIdPart();
+			out.println("<tr>");
+			out.println("    <td><a href=\"" + link + "\">" + component.getVaccineCodeFirstRep().getCodingFirstRep().getCode() + "</a></td>");
+			out.println("    <td><a href=\"" + link + "\">" + component.getDateCriterionFirstRep().getValue() + "</a></td>");
+			out.println("    <td><a href=\"" + link + "\">" + component.getDateCriterionFirstRep().getCode().getCodingFirstRep().getDisplay() + "</a></td>");
+			out.println("</tr>");
 		}
-		out.println("</div>");
 	}
+
 }
