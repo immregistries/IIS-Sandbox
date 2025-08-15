976624f224709460eb50448c47d91800876898ce
Clement Hennequin <clement.hennequin@nist.gov>
2025-08-15 15:58:51 -0400

    Reactivated validationSupport from example

--- src/main/java/org/immregistries/iis/kernal/fhir/common/FhirServerConfigR4.java
+++ src/main/java/org/immregistries/iis/kernal/fhir/common/FhirServerConfigR4.java
@@ -1,8 +1,16 @@
 package org.immregistries.iis.kernal.fhir.common;
 
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.context.support.ConceptValidationOptions;
+import ca.uhn.fhir.context.support.IValidationSupport;
+import ca.uhn.fhir.context.support.ValidationSupportContext;
 import ca.uhn.fhir.jpa.config.r4.JpaR4Config;
+import org.hl7.fhir.common.hapi.validation.support.RemoteTerminologyServiceValidationSupport;
+import org.hl7.fhir.common.hapi.validation.support.ValidationSupportChain;
 import org.immregistries.iis.kernal.fhir.ServerConfig;
 import org.immregistries.iis.kernal.fhir.common.annotations.OnR4Condition;
+import org.immregistries.iis.kernal.fhir.common.validation.OnRemoteTerminologyPresent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -18,52 +26,52 @@ import org.springframework.context.annotation.Import;
 	ServerConfig.class,
 })
 public class FhirServerConfigR4 {
-//
-//	@Bean(name = "myHybridRemoteValidationSupportChain")
-//	@Conditional({OnR4Condition.class, OnRemoteTerminologyPresent.class})
-//	public IValidationSupport addRemoteValidation(
-//		ValidationSupportChain theValidationSupport, FhirContext theFhirContext, AppProperties theAppProperties) {
-//		var values = theAppProperties.getRemoteTerminologyServicesMap().values();
-//
-//		// If the remote terminology service is "*" and is the only one then forward all requests to the remote
-//		// terminology service
-//		if (values.size() == 1 && "*".equalsIgnoreCase(values.iterator().next().getSystem())) {
-//			var remoteSystem = values.iterator().next();
-//			theValidationSupport.addValidationSupport(
-//				0, new RemoteTerminologyServiceValidationSupport(theFhirContext, remoteSystem.getUrl()));
-//			return theValidationSupport;
-//
-//			// If there are multiple remote terminology services, then add each one to the validation chain
-//		} else {
-//			values.forEach((remoteSystem) -> theValidationSupport.addValidationSupport(
-//				0, new RemoteTerminologyServiceValidationSupport(theFhirContext, remoteSystem.getUrl()) {
-//					@Override
-//					public boolean isCodeSystemSupported(
-//						ValidationSupportContext theValidationSupportContext, String theSystem) {
-//						return remoteSystem.getSystem().equalsIgnoreCase(theSystem);
-//					}
-//
-//					@Override
-//					public CodeValidationResult validateCode(
-//						ValidationSupportContext theValidationSupportContext,
-//						ConceptValidationOptions theOptions,
-//						String theCodeSystem,
-//						String theCode,
-//						String theDisplay,
-//						String theValueSetUrl) {
-//						if (remoteSystem.getSystem().equalsIgnoreCase(theCodeSystem)) {
-//							return super.validateCode(
-//								theValidationSupportContext,
-//								theOptions,
-//								theCodeSystem,
-//								theCode,
-//								theDisplay,
-//								theValueSetUrl);
-//						}
-//						return null;
-//					}
-//				}));
-//		}
-//		return theValidationSupport;
-//	}
+
+	@Bean(name = "myHybridRemoteValidationSupportChain")
+	@Conditional({OnR4Condition.class, OnRemoteTerminologyPresent.class})
+	public IValidationSupport addRemoteValidation(
+		ValidationSupportChain theValidationSupport, FhirContext theFhirContext, AppProperties theAppProperties) {
+		var values = theAppProperties.getRemoteTerminologyServicesMap().values();
+
+		// If the remote terminology service is "*" and is the only one then forward all requests to the remote
+		// terminology service
+		if (values.size() == 1 && "*".equalsIgnoreCase(values.iterator().next().getSystem())) {
+			var remoteSystem = values.iterator().next();
+			theValidationSupport.addValidationSupport(
+				0, new RemoteTerminologyServiceValidationSupport(theFhirContext, remoteSystem.getUrl()));
+			return theValidationSupport;
+
+			// If there are multiple remote terminology services, then add each one to the validation chain
+		} else {
+			values.forEach((remoteSystem) -> theValidationSupport.addValidationSupport(
+				0, new RemoteTerminologyServiceValidationSupport(theFhirContext, remoteSystem.getUrl()) {
+					@Override
+					public boolean isCodeSystemSupported(
+						ValidationSupportContext theValidationSupportContext, String theSystem) {
+						return remoteSystem.getSystem().equalsIgnoreCase(theSystem);
+					}
+
+					@Override
+					public CodeValidationResult validateCode(
+						ValidationSupportContext theValidationSupportContext,
+						ConceptValidationOptions theOptions,
+						String theCodeSystem,
+						String theCode,
+						String theDisplay,
+						String theValueSetUrl) {
+						if (remoteSystem.getSystem().equalsIgnoreCase(theCodeSystem)) {
+							return super.validateCode(
+								theValidationSupportContext,
+								theOptions,
+								theCodeSystem,
+								theCode,
+								theDisplay,
+								theValueSetUrl);
+						}
+						return null;
+					}
+				}));
+		}
+		return theValidationSupport;
+	}
 }
