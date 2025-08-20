00df3c2cf8939f638755fdfd21c6ea855f69925e
Clement Hennequin <clement.hennequin@nist.gov>
2025-08-20 13:38:53 -0400

    increased Message store size

--- src/main/java/org/immregistries/iis/kernal/logic/MessageRecordingService.java
+++ src/main/java/org/immregistries/iis/kernal/logic/MessageRecordingService.java
@@ -6,6 +6,7 @@ import org.immregistries.iis.kernal.fhir.security.ServletHelper;
 import org.immregistries.iis.kernal.model.PatientMaster;
 import org.immregistries.iis.kernal.model.persisted.MessageReceived;
 import org.immregistries.iis.kernal.model.persisted.Tenant;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -23,22 +24,30 @@ public class MessageRecordingService {
 	}
 
 	public void recordMessageReceived(String message, PatientMaster patient, String messageResponse, String categoryRequest, String categoryResponse, Tenant tenant) {
+		MessageReceived messageReceived = getMessageReceived(message, patient, messageResponse, categoryRequest, categoryResponse, tenant);
+		recordMessageReceived(messageReceived);
+	}
+
+	private void recordMessageReceived(MessageReceived messageReceived) {
 		try (Session dataSession = ServletHelper.getDataSession()) {
 			Transaction transaction = dataSession.beginTransaction();
-			MessageReceived messageReceived = new MessageReceived();
-			messageReceived.setTenant(tenant);
-			messageReceived.setMessageRequest(message);
-			if (patient != null) {
-				messageReceived.setPatientReportedId(patient.getPatientId());
-			}
-			messageReceived.setMessageResponse(messageResponse);
-			messageReceived.setReportedDate(new Date());
-			messageReceived.setCategoryRequest(categoryRequest);
-			messageReceived.setCategoryResponse(categoryResponse);
-			// TODO interact with internal logs and metadata
 			dataSession.persist(messageReceived);
 			transaction.commit();
 		}
+	}
 
+	private static @NotNull MessageReceived getMessageReceived(String message, PatientMaster patient, String messageResponse, String categoryRequest, String categoryResponse, Tenant tenant) {
+		MessageReceived messageReceived = new MessageReceived();
+		messageReceived.setTenant(tenant);
+		messageReceived.setMessageRequest(message);
+		if (patient != null) {
+			messageReceived.setPatientReportedId(patient.getPatientId());
+		}
+		messageReceived.setMessageResponse(messageResponse);
+		messageReceived.setReportedDate(new Date());
+		messageReceived.setCategoryRequest(categoryRequest);
+		messageReceived.setCategoryResponse(categoryResponse);
+		// TODO interact with internal logs and metadata
+		return messageReceived;
 	}
 }
