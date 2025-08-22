b07997c743bc55d0a7de6a15cd4a12b8dcc24910
Clement Hennequin <clement.hennequin@nist.gov>
2025-08-22 10:18:51 -0400

    Temp disable csrf as UI functionalities are getting closer to Stateless API

--- src/main/java/org/immregistries/iis/kernal/fhir/security/ServerSecurityConfig.java
+++ src/main/java/org/immregistries/iis/kernal/fhir/security/ServerSecurityConfig.java
@@ -1,7 +1,9 @@
 package org.immregistries.iis.kernal.fhir.security;
 
-import org.immregistries.iis.kernal.servlet.*;
-import org.immregistries.iis.kernal.servlet.shlink.ShLinkController;
+import org.immregistries.iis.kernal.servlet.FhirMessagingController;
+import org.immregistries.iis.kernal.servlet.HomeController;
+import org.immregistries.iis.kernal.servlet.PopController;
+import org.immregistries.iis.kernal.servlet.SoapController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -14,12 +16,10 @@ import org.springframework.security.web.savedrequest.RequestCache;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.immregistries.iis.kernal.servlet.LoginController.LOGIN_PARAM_PASSWORD;
 import static org.immregistries.iis.kernal.servlet.LoginController.LOGIN_PARAM_USERID;
-import static org.immregistries.iis.kernal.servlet.shlink.PatientShlinkManifestController.PATIENT_MANIFEST_FULL_PATH;
 import static org.immregistries.iis.kernal.servlet.shlink.ShLinkManifestController.SHLINKS_CONTROLLER_BASE_URL;
 
 
@@ -64,27 +64,27 @@ public class ServerSecurityConfig {
 				.deleteCookies("JSESSIONID")
 			);
 
-		List<RequestMatcher> csrfIgnoringRequestMatchers = new ArrayList<>(30);
-		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, PATIENT_MANIFEST_FULL_PATH + "/**");
-		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, "/manifest/**");
-		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, PopController.POP_BASE_PATH);
-		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, V2ToFhirController.V2_TO_FHIR_BASE_PATH);
-		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, FhirMessagingController.FHIR_MESSAGING_BASE_PATH);
-		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, FhirMessagingController.FHIR_MESSAGING_BASE_PATH + SoapController.SOAP_BASE_PATH);
-		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, MessageController.MESSAGE_BASE_PATH);
-		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, "/fhir/**");
-		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, LOGIN_FORM_PATH);
-		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, LOGIN_PATH);
-		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, LOGOUT_PATH);
-		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, PatientController.PATIENT_BASE_PATH);
-		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, SubscriptionController.SUBSCRIPTION_BASE_PATH);
-		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, VaccinationController.VACCINATION_BASE_PATH);
-		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, TenantController.TENANT_PATH);
-		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, LocationController.LOCATION_BASE_PATH);
-		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, SoapController.SOAP_BASE_PATH);
-		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, ShLinkController.SHLINK_CONTROLLER_BASE_PATH + "/**");
-		http.csrf((csrf) -> csrf
-			.ignoringRequestMatchers(csrfIgnoringRequestMatchers.toArray(new RequestMatcher[csrfIgnoringRequestMatchers.size()]))
+//		List<RequestMatcher> csrfIgnoringRequestMatchers = new ArrayList<>(30);
+//		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, PATIENT_MANIFEST_FULL_PATH + "/**");
+//		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, "/manifest/**");
+//		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, PopController.POP_BASE_PATH);
+//		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, V2ToFhirController.V2_TO_FHIR_BASE_PATH);
+//		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, FhirMessagingController.FHIR_MESSAGING_BASE_PATH);
+//		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, FhirMessagingController.FHIR_MESSAGING_BASE_PATH + SoapController.SOAP_BASE_PATH);
+//		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, MessageController.MESSAGE_BASE_PATH);
+//		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, "/fhir/**");
+//		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, LOGIN_FORM_PATH);
+//		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, LOGIN_PATH);
+//		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, LOGOUT_PATH);
+//		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, PatientController.PATIENT_BASE_PATH);
+//		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, SubscriptionController.SUBSCRIPTION_BASE_PATH);
+//		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, VaccinationController.VACCINATION_BASE_PATH);
+//		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, TenantController.TENANT_PATH);
+//		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, LocationController.LOCATION_BASE_PATH);
+//		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, SoapController.SOAP_BASE_PATH);
+//		addTenantifiedRequestMatcher(csrfIgnoringRequestMatchers, ShLinkController.SHLINK_CONTROLLER_BASE_PATH + "/**");
+		http.csrf((csrf) -> csrf.disable()
+//			.ignoringRequestMatchers(csrfIgnoringRequestMatchers.toArray(new RequestMatcher[csrfIgnoringRequestMatchers.size()]))
 		);
 		// ... other configuration
 		return http.build();
@@ -102,22 +102,10 @@ public class ServerSecurityConfig {
 		return new HttpSessionRequestCache();
 	}
 
-
-	private AntPathRequestMatcher tenantifyRequestMatcher(HttpMethod httpMethod, String pathSuffix) {
-		AntPathRequestMatcher antPathRequestMatcher = tenantifyRequestMatcher(pathSuffix);
-		String tenantified = ServletHelper.tenantifyUrl("*", pathSuffix);
-		return new AntPathRequestMatcher(pathSuffix, httpMethod.toString());
-	}
-
 	private void addTenantifiedRequestMatcher(List<RequestMatcher> matchers, String pathSuffix) {
 		String tenantified = ServletHelper.tenantifyUrl("*", pathSuffix);
 		matchers.add(new AntPathRequestMatcher(pathSuffix));
 		matchers.add(new AntPathRequestMatcher(tenantified));
-	}
-
-	private AntPathRequestMatcher tenantifyRequestMatcher(String pathSuffix) {
-		String tenantified = ServletHelper.tenantifyUrl("*", pathSuffix);
-		return new AntPathRequestMatcher(tenantified);
 	}
 
 }
