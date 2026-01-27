f152776a77257e5fb229bc95419bce735301dc13
clement <clement.hennequin@nist.gov>
2026-01-27 15:35:01 -0500

    Iis web module deploying

---
+++ iis-web/src/main/java/org/immregistries/iis/kernal/ApiUrlServiceImpl.java
@@ -0,0 +1,15 @@
+package org.immregistries.iis.kernal;
+
+import org.immregistries.iis.kernal.logic.api.IApiUrlService;
+import org.immregistries.iis.kernal.persisted.entities.Tenant;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApiUrlServiceImpl implements IApiUrlService {
+
+	public @NotNull String fhirServerBasePath(Tenant tenant) {
+		return Application.fhirServerBasePath(tenant);
+	}
+
+}
--- iis-web/src/main/java/org/immregistries/iis/kernal/ShApiUrlServiceImpl.java
+++ iis-web/src/main/java/org/immregistries/iis/kernal/ShApiUrlServiceImpl.java
@@ -1,14 +1,15 @@
-package org.immregistries.iis.kernal.logic.shlink;
+package org.immregistries.iis.kernal;
 
-import org.immregistries.iis.kernal.Application;
 import org.immregistries.iis.kernal.controllers.IisRestPath;
+import org.immregistries.iis.kernal.logic.shlink.IShApiUrlService;
+import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import static org.immregistries.iis.kernal.controllers.IisPathVariable.Key.CONTENT_ID;
 import static org.immregistries.iis.kernal.controllers.IisPathVariable.Key.MANIFEST_ID;
 import static org.immregistries.iis.kernal.controllers.rest.shlink.ShLinkManifestRestController.SHLINKS_CONTROLLER_REST_BASE_URL;
 
-
+@Service
 public class ShApiUrlServiceImpl implements IShApiUrlService {
 	public void replaceUrlWithShCardPattern(UriComponentsBuilder uriBuilder) {
 		uriBuilder.replacePath(Application.IIS_PATH_BASE + SHLINKS_CONTROLLER_REST_BASE_URL + "/{" + MANIFEST_ID + "}");
@@ -18,7 +19,5 @@ public class ShApiUrlServiceImpl implements IShApiUrlService {
 	public void replaceUrlWithShLinkPattern(UriComponentsBuilder uriBuilder) {
 		uriBuilder.replacePath( Application.IIS_PATH_BASE  + IisRestPath.SHLINK_CONTENT_PATH + "/{" + CONTENT_ID + "}");
 	}
-
-
 
 }
