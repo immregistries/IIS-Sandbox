ab66e895cc1bd545711b7c3bd36d3470035e9349
Clement Hennequin <clement.hennequin@nist.gov>
2026-04-23 00:03:15 -0400

    fix patient manifest url access

--- iis-web/src/main/java/org/immregistries/iis/kernal/controllers/rest/shlink/PatientShLinkManifestRestController.java
+++ iis-web/src/main/java/org/immregistries/iis/kernal/controllers/rest/shlink/PatientShLinkManifestRestController.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
-import static org.immregistries.iis.kernal.controllers.servlet.TenantController.PARAM_TENANT_ID;
+import static org.immregistries.iis.kernal.controllers.servlet.TenantController.PARAM_TENANT_NAME;
 
 @RestController
 @RequestMapping(IisRestPath.MANIFEST_FULL_PATH)
@@ -55,12 +55,12 @@ public class PatientShLinkManifestRestController {
 	@PostMapping({ "/patient", "/patient/{id}" })
 	protected ShLinkManifest postPatientShLinkManifest(HttpServletRequest req, HttpServletResponse resp,
 			@PathVariable(value = "id", required = false) String id,
-			@PathVariable(PARAM_TENANT_ID) String tenantId,
+																		@PathVariable(PARAM_TENANT_NAME) String tenantName,
 																		@RequestAttribute(IisRequestAttribute.TENANT_REQUEST_ATTRIBUTE) Tenant tenant,
 			@RequestBody ShLinkManifestRequestBody body) throws IOException, ServletException {
 		String passcode = body.getPasscode();
 		if (StringUtils.isNotBlank(passcode)) {
-			tenant = tenantAuthService.authenticateTenantNoUsername(tenantId, passcode);
+			tenant = tenantAuthService.authenticateTenantNoUsername(tenantName, passcode);
 		}
 		if (tenant == null) {
 			throw new AuthenticationCredentialsNotFoundException("No tenant found or invalid passcode");
@@ -71,12 +71,12 @@ public class PatientShLinkManifestRestController {
 	@PostMapping({"/patient/ips", "/patient/{id}/ips"})
 	protected ShLinkManifest postPatientIpsShLinkManifest(HttpServletRequest req, HttpServletResponse resp,
 																			@PathVariable(value = "id", required = false) String id,
-																			@PathVariable(PARAM_TENANT_ID) String tenantId,
+																			@PathVariable(PARAM_TENANT_NAME) String tenantName,
 																			@RequestAttribute(IisRequestAttribute.TENANT_REQUEST_ATTRIBUTE) Tenant tenant,
 																			@RequestBody ShLinkManifestRequestBody body) throws IOException, ServletException {
 		String passcode = body.getPasscode();
 		if (StringUtils.isNotBlank(passcode)) {
-			tenant = tenantAuthService.authenticateTenantNoUsername(tenantId, passcode);
+			tenant = tenantAuthService.authenticateTenantNoUsername(tenantName, passcode);
 		}
 		if (tenant == null) {
 			throw new AuthenticationCredentialsNotFoundException("No tenant found or invalid passcode");
