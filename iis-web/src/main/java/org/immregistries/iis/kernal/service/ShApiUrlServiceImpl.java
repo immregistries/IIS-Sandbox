4436bb462f0dfde09cf249bf509fcb341be383d4
Clement Hennequin <clement.hennequin@nist.gov>
2026-04-23 01:52:38 -0400

    removed remnant of useless service

--- iis-web/src/main/java/org/immregistries/iis/kernal/service/ShApiUrlServiceImpl.java
+++ iis-web/src/main/java/org/immregistries/iis/kernal/service/ShApiUrlServiceImpl.java
@@ -1,9 +1,9 @@
 package org.immregistries.iis.kernal.service;
 
-import org.immregistries.iis.kernal.IisConfigService;
 import org.immregistries.iis.kernal.controllers.IisPathVariable;
 import org.immregistries.iis.kernal.controllers.IisRestPath;
 import org.immregistries.iis.kernal.logic.shlink.IShApiUrlService;
+import org.immregistries.iis.kernal.services.api.IDeployedApiUrlService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -13,15 +13,15 @@ import static org.immregistries.iis.kernal.controllers.rest.shlink.ShLinkManifes
 @Service
 public class ShApiUrlServiceImpl implements IShApiUrlService {
 	@Autowired
-	private IisConfigService iisConfigService;
+	private IDeployedApiUrlService deployedUrlService;
 
 	public void replaceUrlWithShCardPattern(UriComponentsBuilder uriBuilder) {
-		uriBuilder.replacePath(iisConfigService.getCONTEXT_PATH() + SHLINKS_CONTROLLER_REST_BASE_URL + IisPathVariable.PlaceHolder.MANIFEST_ID_PLACEHOLDER);
+		uriBuilder.replacePath(deployedUrlService.getContextPath() + SHLINKS_CONTROLLER_REST_BASE_URL + IisPathVariable.PlaceHolder.MANIFEST_ID_PLACEHOLDER);
 	}
 
 
 	public void replaceUrlWithShLinkPattern(UriComponentsBuilder uriBuilder) {
-		uriBuilder.replacePath(iisConfigService.getCONTEXT_PATH() + IisRestPath.SHLINK_CONTENT_PATH + IisPathVariable.PlaceHolder.CONTENT_ID_PLACEHOLDER);
+		uriBuilder.replacePath(deployedUrlService.getContextPath() + IisRestPath.SHLINK_CONTENT_PATH + IisPathVariable.PlaceHolder.CONTENT_ID_PLACEHOLDER);
 	}
 
 }
