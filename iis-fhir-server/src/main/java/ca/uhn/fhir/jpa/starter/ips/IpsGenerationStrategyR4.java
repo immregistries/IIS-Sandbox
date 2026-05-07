b1754a8c158d00f4e30eca4bbbbd6b14c82990b9
Clement Hennequin <clement.hennequin@nist.gov>
2026-05-07 08:58:44 -0400

    IPS Clean

--- iis-fhir-server/src/main/java/ca/uhn/fhir/jpa/starter/ips/IpsGenerationStrategyR4.java
+++ iis-fhir-server/src/main/java/ca/uhn/fhir/jpa/starter/ips/IpsGenerationStrategyR4.java
@@ -1,14 +1,10 @@
 package ca.uhn.fhir.jpa.starter.ips;
 
-import ca.uhn.fhir.jpa.ips.api.IpsSectionContext;
 import ca.uhn.fhir.jpa.ips.api.Section;
 import ca.uhn.fhir.jpa.ips.jpa.DefaultJpaIpsGenerationStrategy;
 import ca.uhn.fhir.jpa.ips.jpa.JpaSectionSearchStrategyCollection;
-import ca.uhn.fhir.jpa.model.util.JpaConstants;
 import ca.uhn.fhir.jpa.provider.BaseJpaResourceProviderPatient;
-import org.apache.commons.lang3.StringUtils;
 import org.hl7.fhir.instance.model.api.IAnyResource;
-import org.hl7.fhir.instance.model.api.IBaseBundle;
 import org.hl7.fhir.instance.model.api.IIdType;
 import org.hl7.fhir.r4.model.*;
 import org.immregistries.iis.kernal.mapping.IisFhirClientFactory;
@@ -16,7 +12,6 @@ import org.immregistries.iis.kernal.mapping.mappers.resources.r4.OrganizationMap
 import org.immregistries.iis.kernal.security.RequestTenantUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 
@@ -79,23 +74,6 @@ public class IpsGenerationStrategyR4 extends DefaultJpaIpsGenerationStrategy imp
 	public IAnyResource createAuthor() {
 		Organization organization = organizationMapper.fhirObject(requestTenantUtil.extractTenantFromRequestContext());
 		return organization;
-	}
-
-	public IBaseBundle everything(IIdType theOriginalSubjectId, Section theSection) {
-		Parameters inParams = new Parameters();
-		inParams.addParameter("_mdm", true);
-		inParams.addParameter("type", StringUtils.join(theSection.getResourceTypes(), ","));
-		Bundle bundle = iisFhirClientFactory.getOrCreateFhirClientFromContext().operation().onServer().named(JpaConstants.OPERATION_EVERYTHING).withParameters(inParams)
-			.returnResourceType(Bundle.class).execute();
-		return bundle;
-	}
-
-
-	public List<IAnyResource> extractResourcesFromBundle(IpsSectionContext theIpsSectionContext, IBaseBundle iBaseBundle) {
-		Bundle bundle = (Bundle) iBaseBundle;
-		return bundle.getEntry().stream()
-			.filter((bundleEntryComponent -> bundleEntryComponent.hasResource() && theIpsSectionContext.getResourceType().equals(bundleEntryComponent.getResource().getResourceType().name())))
-			.map(Bundle.BundleEntryComponent::getResource).collect(Collectors.toList());
 	}
 
 	public String mdmLinksParameterIds(IIdType theOriginalSubjectId, Section theSection) {
--- iis-fhir-server/src/main/java/ca/uhn/fhir/jpa/starter/ips/IpsGenerationStrategyR5.java
+++ iis-fhir-server/src/main/java/ca/uhn/fhir/jpa/starter/ips/IpsGenerationStrategyR5.java
@@ -1,24 +1,17 @@
 package ca.uhn.fhir.jpa.starter.ips;
 
-import ca.uhn.fhir.jpa.ips.api.IpsSectionContext;
 import ca.uhn.fhir.jpa.ips.api.Section;
 import ca.uhn.fhir.jpa.ips.jpa.DefaultJpaIpsGenerationStrategy;
 import ca.uhn.fhir.jpa.ips.jpa.JpaSectionSearchStrategyCollection;
-import ca.uhn.fhir.jpa.model.util.JpaConstants;
 import ca.uhn.fhir.jpa.provider.BaseJpaResourceProviderPatient;
-import org.apache.commons.lang3.StringUtils;
 import org.hl7.fhir.instance.model.api.IAnyResource;
-import org.hl7.fhir.instance.model.api.IBaseBundle;
-import org.hl7.fhir.instance.model.api.IIdType;
-import org.hl7.fhir.r5.model.*;
+import org.hl7.fhir.r5.model.Immunization;
+import org.hl7.fhir.r5.model.Organization;
+import org.hl7.fhir.r5.model.Patient;
 import org.immregistries.iis.kernal.mapping.IisFhirClientFactory;
 import org.immregistries.iis.kernal.mapping.mappers.resources.r5.OrganizationMapperR5;
 import org.immregistries.iis.kernal.security.RequestTenantUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
 
 
 public class IpsGenerationStrategyR5 extends DefaultJpaIpsGenerationStrategy implements ICustomIpsGenerationStrategy {
@@ -80,31 +73,6 @@ public class IpsGenerationStrategyR5 extends DefaultJpaIpsGenerationStrategy imp
 	public IAnyResource createAuthor() {
 		Organization organization = organizationMapper.fhirObject(requestTenantUtil.extractTenantFromRequestContext());
 		return organization;
-	}
-
-	public IBaseBundle everything(IIdType theOriginalSubjectId, Section theSection) {
-		Parameters inParams = new Parameters();
-		inParams.addParameter("_mdm", true);
-		inParams.addParameter("type", StringUtils.join(theSection.getResourceTypes(), ","));
-		Bundle bundle = iisFhirClientFactory.getOrCreateFhirClientFromContext().operation().onServer().named(JpaConstants.OPERATION_EVERYTHING).withParameters(inParams)
-			.returnResourceType(Bundle.class).execute();
-		return bundle;
-	}
-
-
-	public List<IAnyResource> extractResourcesFromBundle(IpsSectionContext theIpsSectionContext, IBaseBundle iBaseBundle) {
-		Bundle bundle = (Bundle) iBaseBundle;
-		return bundle.getEntry().stream()
-			.filter((bundleEntryComponent -> bundleEntryComponent.hasResource() && theIpsSectionContext.getResourceType().equals(bundleEntryComponent.getResource().getResourceType().name())))
-			.map(Bundle.BundleEntryComponent::getResource).collect(Collectors.toList());
-	}
-
-	public String mdmLinksParameterIds(IIdType theOriginalSubjectId, Section theSection) {
-		Parameters inParams = new Parameters();
-		inParams.addParameter("resourceId", theOriginalSubjectId.getValue());
-		Bundle bundle = iisFhirClientFactory.getOrCreateFhirClientFromContext().operation().onServer().named("$mdm-query-links").withParameters(inParams)
-			.returnResourceType(Bundle.class).execute();
-		return bundle.getEntry().stream().map(bundleEntryComponent -> bundleEntryComponent.getResource().getId()).collect(Collectors.joining(","));
 	}
 
 }
